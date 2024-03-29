08061284aabd353be525368286c3dc59e83db779
imBilalx <97373943+imBilalx@users.noreply.github.com>
2024-03-29 11:44:33 -0400

    Added first Trine Univeristy web scraping logic

---
+++ sam_tutorial_demo/Schools/TrineUniversity/TrineUniversity.py
@@ -0,0 +1,25 @@
+from .Tuition.Tuition import TUITION_run
+
+
+class TrineUniversityScraper:
+    def __init__(self):
+        pass
+
+    def scrape(self, info):
+        data = {}
+
+        tuition_url = info.get("tuition")
+        if tuition_url:
+            data['tuition'] = TUITION_run(tuition_url)
+
+
+        return data
+
+    #
+    # # Save dataframe to S3
+    # S3FileSystem = s3fs.S3FileSystem(anon=False)
+    # with S3FileSystem.open(f's3://your-bucket-name/{TABLE}.csv', 'w') as file:
+    #     df.to_csv(file)
+    #
+    # # can return some info if required
+    # return f"Scraped and saved data for {TABLE}"
--- sam_tutorial_demo/app.py
+++ sam_tutorial_demo/app.py
@@ -1,20 +1,35 @@
 import json
-import pandas as pd
+import os
+from Schools.TrineUniversity.TrineUniversity import TrineUniversityScraper
+
+
+class SchoolScraper:
+    def __init__(self):
+        self.school_function_map = {
+            'TrineUniversity': TrineUniversityScraper(),
+        }
+
+    def scrape_and_save(self, event):
+        for school in event['schools']:
+            school_name = school['name']
+            info = {"tuition": school["tuition"]}
+            scraper_function = self.school_function_map.get(school_name)
+            if scraper_function:
+                result = scraper_function.scrape(info)
+                print(result)
+            else:
+                print({"error": f"No scraping function found for school: {school_name}"})
 
 
 def lambda_handler(event, context):
-    count = event['count']
-    data = []
-    for i in range(count):
-        data.append(i * 10)
-        df = pd.DataFrame(data, columns=['Numbers'])
-        print('Data BRO:')
-        print(df)
+    scraper = SchoolScraper()
+    scraper.scrape_and_save(event)
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Success",
-            "count": count,
-        }),
-    }
+
+if __name__ == "__main__":
+    parent_directory = os.path.dirname(os.getcwd())
+    event_path = os.path.join(parent_directory, 'events', 'event.json')
+    with open(event_path, 'r') as file:
+        event_data = json.load(file)
+    scraper_ = SchoolScraper()
+    print(scraper_.scrape_and_save(event_data))
