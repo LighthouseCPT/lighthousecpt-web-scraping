d0b09a95a1f863a235c253275c2b6199424c59f4
imBilalx <97373943+imBilalx@users.noreply.github.com>
2024-04-02 12:45:30 -0400

    added headers to request to avoid 403

--- WebScrapingProject/Schools/utils.py
+++ WebScrapingProject/Schools/utils.py
@@ -9,7 +9,9 @@ logger = configure_logger(__name__)
 
 def make_request(url):
     try:
-        page = requests.get(url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, '
+                                 'like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        page = requests.get(url, headers=headers)
         logger.info(f'Successfully made request to: {url}')
         return page
     except requests.RequestException as e:
