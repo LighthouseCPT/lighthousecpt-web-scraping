d58126546b927ce797c6c21550ce87bd47b8fa36
imBilalx <97373943+imBilalx@users.noreply.github.com>
2024-06-11 19:10:41 -0400

    updated TrineUniveristy

--- SchoolScrapingProject/Schools/TrineUniversity/tuition/tuition.py
+++ SchoolScrapingProject/Schools/TrineUniversity/tuition/tuition.py
@@ -4,6 +4,6 @@ from Schools.utils import extract_content, extract_inner_string
 
 def TrineUniversity_tuition(source):
     soup = BeautifulSoup(source, 'html.parser')
-    x = extract_content(soup, 'International Tuition and Fees - Graduate Degrees', 'Take the Next Steps')
+    x = extract_content(soup, 'International Tuition and Fees - Hybrid Degrees', 'Take the Next Steps')
     y = extract_inner_string(x, "Students are responsible", "visa denial.", include_end=True)
     return y
